49f56f615096d842aa59e6a99589e77f41b255d3
blizza4d <arkdharun7203@gmail.com>
2026-05-27 23:23:43 +0530

    run

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -49,20 +49,20 @@ function Shop(){
     </div><br></br><br/>
         <div id = 'item_cont'>
     <div className="item">
-        <img src='src\assets\Urea.jpg'></img>
+        <img src='src/assets/Urea.jpg'></img>
         <h2>Urea</h2>
         <h3>70$</h3>
         <button id='stat' onClick={() => upd(1)}>{cart.has(1) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
     </div>
     <div className="item">
-        <img src='src\assets\Potash.jpg'></img>
+        <img src='src/assets/Potash.jpg'></img>
         <h2>Potash fertilizer</h2>
         <h3>100$</h3>
         <button onClick={() => upd(2)}>{cart.has(2) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
     </div>
 
     <div className="item">
-        <img src='src\assets\Dap.jpg'></img>
+        <img src='src/assets/Dap.jpg'></img>
         <h2>Diammonium Phosphate</h2>
         <h3>120$</h3>
         <button onClick={() => upd(3)}>{cart.has(3) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
@@ -70,26 +70,26 @@ function Shop(){
     </div>
 
     <div className="item">
-        <img src='src\assets\Mop.jpg'></img>
+        <img src='src/assets/Mop.jpg'></img>
         <h2>Muriate of Potash</h2>
         <h3>150$</h3>
         <button onClick={() => upd(4)}>{cart.has(4) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
     </div>
     
     <div className="item">
-        <img src='src\assets\Can.jpg'></img>
+        <img src='src/assets/Can.jpg'></img>
         <h2>Calcium ammonium nitrate</h2>
         <h3>50$</h3>
         <button onClick={() => upd(5)}>{cart.has(5) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
     </div>
     <div className="item">
-        <img src='src\assets\Ammonia.jpg'></img>
+        <img src='src/assets/Ammonia.jpg'></img>
         <h2>Ammonium sulphate</h2>
         <h3>30$</h3>
         <button onClick={() => upd(6)}>{cart.has(6) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
     </div>
     <div className="item">
-        <img src='src\assets\Potassi.jpg'></img>
+        <img src='src/assets/Potassi.jpg'></img>
         <h2>Potassium sulphate</h2>
        <h3>45$</h3>
         <button onClick={() => upd(7)}>{cart.has(7) ? "REMOVE FROM CART" : "ADD TO CART"}</button>
